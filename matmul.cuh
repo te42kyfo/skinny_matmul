d0aa0a4daaadbc063de0ebfb5461d722ecf088d2
Dominik Ernst <dominik.ernst@fau.de>
2015-09-02 19:30:37 +0200

    automatic MxKxN instantiation and dispatch

--- matmul.cuh
+++ matmul.cuh
@@ -229,36 +229,72 @@ void MXN(size_t &temp_storage_bytes, double *d_temp_storage, double *A,
 }
 }
 
-template <int MMAX>
-void matmul_dispatch(size_t &temp_storage_bytes, double *d_temp_storage,
-                     double *A, double *B, double *result, const size_t M,
-                     const size_t N, const size_t K, const int blockCount) {
-  if (M == MMAX) {
-    MXN::MXN<MMAX, 1>(temp_storage_bytes, d_temp_storage, A, B, result, K,
-                      blockCount);
-  } else if (M > 1) {
-    matmul_dispatch<MMAX - 1>(temp_storage_bytes, d_temp_storage, A, B, result,
-                              M, N, K, blockCount);
+template <int MMAX, int NMAX>
+struct matmul_dispatch {
+  void static m(size_t &temp_storage_bytes, double *d_temp_storage, double *A,
+                double *B, double *result, const size_t M, const size_t N,
+                const size_t K, const int blockCount) {
+    if (M > MMAX || N > NMAX) {
+      std::cout << "No instantiated matmul variant for " << M << "xKx" << N
+                << "\n";
+      exit(1);
+    }
+    if (M == MMAX) {
+      matmul_dispatch<MMAX, NMAX>::n(temp_storage_bytes, d_temp_storage, A, B,
+                                     result, M, N, K, blockCount);
+    } else {
+      matmul_dispatch<MMAX - 1, NMAX>::m(temp_storage_bytes, d_temp_storage, A,
+                                         B, result, M, N, K, blockCount);
+    }
   }
-}
+  void static n(size_t &temp_storage_bytes, double *d_temp_storage, double *A,
+                double *B, double *result, const size_t M, const size_t N,
+                const size_t K, const int blockCount) {
+    if (N == NMAX) {
+      MXN::MXN<MMAX, NMAX>(temp_storage_bytes, d_temp_storage, A, B, result, K,
+                           blockCount);
+    } else {
+      matmul_dispatch<MMAX, NMAX - 1>::n(temp_storage_bytes, d_temp_storage, A,
+                                         B, result, M, N, K, blockCount);
+    }
+  }
+};
 
-template <>
-void matmul_dispatch<1>(size_t &temp_storage_bytes, double *d_temp_storage,
-                        double *A, double *B, double *result, const size_t M,
-                        const size_t N, const size_t K, const int blockCount) {
-  MXN::MXN<1, 1>(temp_storage_bytes, d_temp_storage, A, B, result, K,
-                 blockCount);
-}
+template <int MMAX>
+struct matmul_dispatch<MMAX, 0> {
+  void static m(size_t &temp_storage_bytes, double *d_temp_storage, double *A,
+                double *B, double *result, const size_t M, const size_t N,
+                const size_t K, const int blockCount) {
+    std::cout << "Invalid zero or negative Matrix Size\n";
+    exit(1);
+  }
+  void static n(size_t &temp_storage_bytes, double *d_temp_storage, double *A,
+                double *B, double *result, const size_t M, const size_t N,
+                const size_t K, const int blockCount) {
+    std::cout << "Invalid zero or negative Matrix Size\n";
+    exit(1);
+  }
+};
+
+template <int NMAX>
+struct matmul_dispatch<0, NMAX> {
+  void static m(size_t &temp_storage_bytes, double *d_temp_storage, double *A,
+                double *B, double *result, const size_t M, const size_t N,
+                const size_t K, const int blockCount) {
+    std::cout << "Invalid zero or negative Matrix Size\n";
+    exit(1);
+  }
+  void static n(size_t &temp_storage_bytes, double *d_temp_storage, double *A,
+                double *B, double *result, const size_t M, const size_t N,
+                const size_t K, const int blockCount) {
+    std::cout << "Invalid zero or negative Matrix Size\n";
+    exit(1);
+  }
+};
 
 void matmul(size_t &temp_storage_bytes, double *d_temp_storage, double *A,
             double *B, double *result, const size_t M, const size_t N,
             const size_t K, const int blockCount) {
-  if (N == 1 && M < 10) {
-    matmul_dispatch<10>(temp_storage_bytes, d_temp_storage, A, B, result, M, N,
-                        K, blockCount);
-    return;
-  }
-
-  std::cout << "No matmul variant for " << M << "xKx" << N << "\n";
-  exit(1);
+  matmul_dispatch<10, 10>::m(temp_storage_bytes, d_temp_storage, A, B, result,
+                             M, N, K, blockCount);
 }
