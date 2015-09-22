1c996f070bc331a55a982f00eaba5d6b1c1ca642
Dominik Ernst <dominik.ernst@fau.de>
2015-09-22 16:17:49 +0200

    switch to genv2

--- matmul.cu
+++ matmul.cu
@@ -6,7 +6,7 @@ void matmul(size_t &temp_storage_bytes, double *d_temp_storage, double *A,
             double *B, double *result, const size_t M, const size_t N,
             const size_t K, const int blockCount) {
   if (M == PARM && N == PARN) {
-    GENV3::matmul<PARM, PARN>(temp_storage_bytes, d_temp_storage, A, B, result,
+    GENV2::matmul<PARM, PARN>(temp_storage_bytes, d_temp_storage, A, B, result,
                               K, blockCount);
   } else {
     std::cout << M << "," << N << " does not match instantiated " << PARM << ","
