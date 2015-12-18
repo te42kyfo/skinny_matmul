78005cf9f2f13f7f06cc89accc282b441a8f8faf
Dominik Ernst <dominik.ernst@fau.de>
2015-12-18 16:07:53 +0100

    Implement ldx and alpha/beta in genv5

--- genv5.cuh
+++ genv5.cuh
@@ -6,35 +6,29 @@
 
 namespace GENV5 {
 
-__device__ inline double double_shfl_xor(double var, unsigned int srcLane,
-                                         int width = 32) {
-  int2 a = *reinterpret_cast<int2 *>(&var);
-  a.x = __shfl_xor(a.x, srcLane, width);
-  a.y = __shfl_xor(a.y, srcLane, width);
-  return *reinterpret_cast<double *>(&a);
-}
-
 template <int M, int N>
-__global__ void deviceReduce(double *blockResults, double *result,
-                             int blockCount) {
-  size_t tidx = blockDim.x * blockIdx.x + threadIdx.x;
+__global__ void deviceReduce(double *blockResults, double *result, double alpha,
+                             double beta, int blockCount, int lda, int ldb,
+                             int ldc) {
+  int tidx = blockDim.x * blockIdx.x + threadIdx.x;
 
   if (tidx >= M * N) return;
 
   int n = tidx / M;
   int m = tidx % M;
 
-  double sum = 0;
+  double sum = 0.0;
   for (int i = 0; i < blockCount; i++) {
-    sum += blockResults[i * M * N + n * M + m];
+    sum += blockResults[i * M * ldc + m * ldc + n];
   }
 
-  result[n * M + m] = sum;
+  result[m * ldc + n] = result[m * ldc + n] * beta + sum * alpha;
 }
 
 template <int M, int N, int BLOCKSIZE, bool TRANSPOSE>
-__global__ void blockProductKernel(double *A, double *B, double *out,
-                                   size_t K) {
+__global__ void blockProductKernel(const double *A, const double *B,
+                                   double *out, int K, const int lda,
+                                   const int ldb, const int ldc) {
   int const warpsPerColumn = (M - 1) / 32 + 1;
   int const columnsPerBlock = BLOCKSIZE / 32 / warpsPerColumn;
 
@@ -58,11 +52,19 @@ __global__ void blockProductKernel(double *A, double *B, double *out,
   for (size_t k =
            blockIdx.x * columnsPerBlock + threadIdx.x / warpsPerColumn / 32;
        k < K; k += gridDim.x * columnsPerBlock) {
-    for (int n = warpLane; n < N; n += 32) {
-      rowCache[warpId][n] = B[k * N + n];
-    }
-    for (int n = 0; n < N; n++) {
-      threadSum[n] += A[k * M + m] * rowCache[warpId][n];
+    // Split of N=1 as a special case, because warp divergence can lead to
+    // hazards in the row cache
+    if (N > 1) {
+      for (int n = warpLane; n < N; n += 32) {
+        rowCache[warpId][n] = B[k * ldb + n];
+      }
+      for (int n = 0; n < N; n++) {
+        threadSum[n] += A[k * lda + m] * rowCache[warpId][n];
+      }
+    } else {
+      for (int n = 0; n < N; n++) {
+        threadSum[n] += A[k * lda + m] * B[k * ldb + n];
+      }
     }
   }
 
@@ -77,23 +79,33 @@ __global__ void blockProductKernel(double *A, double *B, double *out,
            i += 32 * warpsPerColumn) {
         blockSum += blockStorage[i];
       }
-      out[blockIdx.x * M * N + n * M + m] = blockSum;
+      if (TRANSPOSE) {
+        out[blockIdx.x * N * ldc + n * ldc + m] = blockSum;
+      } else {
+        out[blockIdx.x * M * ldc + m * ldc + n] = blockSum;
+      }
     }
   }
 }
 
 template <int M, int N>
-void matmul(size_t &temp_storage_bytes, double *d_temp_storage, double *A,
-            double *B, double *result, const size_t K, const int blockCount) {
+void matmul(size_t &temp_storage_bytes, double *d_temp_storage,
+            const int blockCount, const int K, const double alpha,
+            const double *A, const int lda, const double *B, const int ldb,
+            const double beta, double *C, const int ldc) {
   if (temp_storage_bytes == 0) {
-    temp_storage_bytes = blockCount * sizeof(double) * M * N;
+    temp_storage_bytes = blockCount * sizeof(double) * M * ldc;
     return;
   }
 
-  GENV5::blockProductKernel<M, N, 256, false><<<blockCount, 256>>>(
-      A, B, d_temp_storage, K);
-
-  GENV5::deviceReduce<M, N><<<M * N / 256 + 1, 256>>>(d_temp_storage, result,
-                                                      blockCount);
+  if (N > M) {
+    GENV5::blockProductKernel<N, M, 256, true><<<blockCount, 256>>>(
+        B, A, d_temp_storage, K, ldb, lda, ldc);
+  } else {
+    GENV5::blockProductKernel<M, N, 256, false><<<blockCount, 256>>>(
+        A, B, d_temp_storage, K, lda, ldb, ldc);
+  }
+  GENV5::deviceReduce<M, N><<<M * N / 256 + 1, 256>>>(
+      d_temp_storage, C, alpha, beta, blockCount, lda, ldb, ldc);
 }
 }
