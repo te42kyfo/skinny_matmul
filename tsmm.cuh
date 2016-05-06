ee99a4572a304fd38f765f02aff417d04f0d347c
Dominik Ernst <dominik.ernst@fau.de>
2016-05-06 16:33:08 +0200

    tsmm: warp reduce variant

--- tsmm.cuh
+++ tsmm.cuh
@@ -17,11 +17,46 @@ static __global__ void ghost_tsmm_cu_rm_cm(const T *A, const T *__restrict__ B,
     }
 
     T sum = 0;
-    for (int m = 0; m < M; m ++) {
+    for (int m = 0; m < M; m++) {
       sum += rowCache[localRow * M + m] * B[m * ldb + n];
     }
 
     out[row * ldc + n] = sum;
+  }
+}
+
+template <typename T>
+__device__ inline T __shfl_xor_t(T var, unsigned int srcLane, int width = 32) {
+  int *a = reinterpret_cast<int *>(&var);
+  for (int i = 0; i < sizeof(T) / 4; i++) {
+    a[i] = __shfl_xor(a[i], srcLane, width);
+  }
+  return *reinterpret_cast<T *>(a);
+}
+
+template <typename T>
+__device__ inline T warpReduce(T lval, int width) {
+  for (int offset = width / 2; offset > 0; offset /= 2) {
+    lval = accu(lval, __shfl_xor_t(lval, offset, width));
+  }
+  return lval;
+}
+
+template <typename T, int M, int N, int BLOCKSIZE, bool betaiszero>
+static __global__ void ghost_tsmm_cu_rm_cm2(const T *__restrict__ A,
+                                            const T *__restrict__ B,
+                                            T *__restrict__ out, const int K,
+                                            const int lda, const int ldb,
+                                            const int ldc) {
+  int m = threadIdx.x % M;
+  int tidx = blockIdx.x * BLOCKSIZE + threadIdx.x;
+
+  for (int row = tidx / M; row < K; row += gridDim.x * BLOCKSIZE / M) {
+    T vA = A[row * lda + m];
+    for (int n = 0; n < N; n++) {
+      T tval = vA * B[m * ldb + n];
+      out[row * ldc + n] = warpReduce(tval, M);
+    }
   }
 }
 
@@ -30,6 +65,6 @@ void tsmm(const size_t blockCount, const int K, const T alpha, const T *A,
           const int lda, const T *B, const int ldb, const T beta, T *C,
           const int ldc) {
   const int BLOCKSIZE = 256;
-  ghost_tsmm_cu_rm_cm<T, M, N, BLOCKSIZE, false><<<blockCount, BLOCKSIZE>>>(
+  ghost_tsmm_cu_rm_cm2<T, M, N, BLOCKSIZE, false><<<blockCount, BLOCKSIZE>>>(
       A, B, C, K, lda, ldb, ldc);
 }
