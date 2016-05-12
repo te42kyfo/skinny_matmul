5d883e81c87ad64dbe00ab7c60186512ef7e9497
Dominik Ernst <dominik.ernst@fau.de>
2016-05-12 14:57:13 +0200

    tsmm: introduce fallback, dispatch

--- tsmm.cuh
+++ tsmm.cuh
@@ -1,14 +1,17 @@
 #include "cu_complex.h"
 
+namespace {
+
 template <typename T, int M, int N, int BLOCKSIZE, bool betaiszero>
-static __global__ void tsmm_fallback(const T *A, const T *__restrict__ B,
-                                     T *out, const int K, const int lda,
-                                     const int ldb, const int ldc) {
+static __global__ void tsmm_fallback_kernel(const T *A, const T *__restrict__ B,
+                                            T *out, const int K, const int lda,
+                                            const int ldb, const int ldc) {
   int tidx = blockIdx.x * BLOCKSIZE + threadIdx.x;
-  int n = threadIdx.x % N;
+  int n = tidx % N;
 
   for (int row = tidx / N; row < K; row += gridDim.x * BLOCKSIZE / N) {
-    T sum = 0;
+    T sum;
+    zero(sum);
     for (int m = 0; m < M; m++) {
       sum += A[row * lda + m] * B[m * ldb + n];
     }
@@ -17,15 +20,15 @@ static __global__ void tsmm_fallback(const T *A, const T *__restrict__ B,
 }
 
 template <typename T, int M, int N, int BLOCKSIZE, bool betaiszero>
-static __global__ void ghost_tsmm_cu_rm_cm(const T *A, const T *__restrict__ B,
-                                           T *out, const int K, const int lda,
-                                           const int ldb, const int ldc) {
+static __global__ void tsmm_v1_kernel(const T *A, const T *__restrict__ B,
+                                      T *out, const int K, const int lda,
+                                      const int ldb, const int ldc) {
   int tidx = blockIdx.x * BLOCKSIZE + threadIdx.x;
   int n = threadIdx.x % N;
 
   int localRow = threadIdx.x / N;
 
-  T __shared__ rowCache[M * BLOCKSIZE / N];
+  T __shared__ rowCache[(M / N < 4) ? M * BLOCKSIZE / N : 1];
 
   for (int row = tidx / N; row < K; row += gridDim.x * BLOCKSIZE / N) {
     for (int i = 0; i < M / N; i++) {
@@ -59,11 +62,11 @@ __device__ inline T warpReduce(T lval, int width) {
 }
 
 template <typename T, int M, int N, int BLOCKSIZE, bool betaiszero>
-static __global__ void ghost_tsmm_cu_rm_cm2(const T *__restrict__ A,
-                                            const T *__restrict__ B,
-                                            T *__restrict__ out, const int K,
-                                            const int lda, const int ldb,
-                                            const int ldc) {
+static __global__ void tsmm_v2_kernel(const T *__restrict__ A,
+                                      const T *__restrict__ B,
+                                      T *__restrict__ out, const int K,
+                                      const int lda, const int ldb,
+                                      const int ldc) {
   const int GANGSIZE = 32 / sizeof(T);
 
   int gId = threadIdx.x % GANGSIZE;
@@ -83,21 +86,57 @@ static __global__ void ghost_tsmm_cu_rm_cm2(const T *__restrict__ A,
     }
   }
 }
+template <typename T, int M, int N>
+bool tsmm_fallback(const size_t blockCount, const int K, const T alpha,
+                   const T *A, const int lda, const T *B, const int ldb,
+                   const T beta, T *C, const int ldc) {
+  const int BLOCKSIZE = 256;
+  tsmm_fallback_kernel<T, M, N, BLOCKSIZE, false><<<blockCount, BLOCKSIZE>>>(
+      A, B, C, K, lda, ldb, ldc);
+  return true;
+}
 
 template <typename T, int M, int N>
-void tsmm(const size_t blockCount, const int K, const T alpha, const T *A,
+bool tsmm_v1(const size_t blockCount, const int K, const T alpha, const T *A,
+             const int lda, const T *B, const int ldb, const T beta, T *C,
+             const int ldc) {
+  const int BLOCKSIZE = 256;
+
+  if (M % N == 0 && (M & (M - 1)) == 0 && M / N < 3) {
+    tsmm_v1_kernel<T, M, N, BLOCKSIZE, false><<<blockCount, BLOCKSIZE>>>(
+        A, B, C, K, lda, ldb, ldc);
+    return true;
+  }
+
+  return false;
+}
+
+template <typename T, int M, int N>
+bool tsmm_v2(const size_t blockCount, const int K, const T alpha, const T *A,
+             const int lda, const T *B, const int ldb, const T beta, T *C,
+             const int ldc) {
+  const int BLOCKSIZE = 256;
+
+  if (M > 32 / sizeof(T)) {
+    tsmm_v2_kernel<T, M, N, BLOCKSIZE, false><<<blockCount, BLOCKSIZE>>>(
+        A, B, C, K, lda, ldb, ldc);
+    return true;
+  }
+
+  return false;
+}
+}
+
+template <typename T, int M, int N>
+bool tsmm(const size_t blockCount, const int K, const T alpha, const T *A,
           const int lda, const T *B, const int ldb, const T beta, T *C,
           const int ldc) {
-  const int BLOCKSIZE = 256;
-  if (M == N) {
-    ghost_tsmm_cu_rm_cm<T, M, N, BLOCKSIZE, false><<<blockCount, BLOCKSIZE>>>(
-        A, B, C, K, lda, ldb, ldc);
-  } else if (M > 32 / sizeof(T)) {
-    ghost_tsmm_cu_rm_cm2<T, M, N, BLOCKSIZE, false><<<blockCount, BLOCKSIZE>>>(
-        A, B, C, K, lda, ldb, ldc);
-  } else {
-    const int BLOCKSIZE = 256;
-    tsmm_fallback<T, M, N, BLOCKSIZE, false><<<blockCount, BLOCKSIZE>>>(
-        A, B, C, K, lda, ldb, ldc);
-  }
+  if (tsmm_v1<T, M, N>(blockCount, K, alpha, A, lda, B, ldb, beta, C, ldc))
+    return true;
+  if (tsmm_v2<T, M, N>(blockCount, K, alpha, A, lda, B, ldb, beta, C, ldc))
+    return true;
+  if (tsmm_fallback<T, M, N>(blockCount, K, alpha, A, lda, B, ldb, beta, C,
+                             ldc))
+    return true;
+  return false;
 }
