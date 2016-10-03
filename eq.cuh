fd16e98d9a1de94cf1f5d875034028931bdc19d6
Dominik Ernst <dominik.ernst@fau.de>
2016-10-03 17:28:16 +0200

    move tsmm/tsmttsm into different folders

---
+++ eq.cuh
@@ -0,0 +1,17 @@
+#pragma once
+#include "cu_complex.cuh"
+
+template <typename T>
+bool eq(const T lhs, const T rhs) {
+  return lhs == rhs;
+};
+
+template <>
+bool eq<cuDoubleComplex>(const cuDoubleComplex lhs, const cuDoubleComplex rhs) {
+  return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+template <>
+bool eq<cuFloatComplex>(const cuFloatComplex lhs, const cuFloatComplex rhs) {
+  return lhs.x == rhs.x && lhs.y == rhs.y;
+}
--- matmul.cuh
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void matmul(size_t &temp_storage_bytes, double *d_temp_storage, double *A,
-            double *B, double *result, const size_t M, const size_t N,
-            const size_t K, const int blockCount);
