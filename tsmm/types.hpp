1676a735724c602f528448d9d1bb7b92b666634f
Dominik Ernst <dominik.ernst@fau.de>
2016-10-18 12:54:54 +0200

    types: replace long type names by short abbreviation

--- tsmm/types.hpp
+++ tsmm/types.hpp
@@ -8,7 +8,7 @@ typedef cuFloatComplex dtype;
 dtype makeDtype(htype v) { return make_cuFloatComplex(v.real(), v.imag()); }
 #define RAND_HTYPE(gen) htype(gen, gen)
 #define MAKE_DTYPE(v1, v2) make_cuFloatComplex(v1, v2)
-std::string mode = "float complex";
+std::string mode = "FC";
 int flopsPerCell = 8;
 
 #elif DC
@@ -17,7 +17,7 @@ typedef cuDoubleComplex dtype;
 dtype makeDtype(htype v) { return make_cuDoubleComplex(v.real(), v.imag()); }
 #define RAND_HTYPE(gen) htype(gen, gen)
 #define MAKE_DTYPE(v1, v2) make_cuDoubleComplex(v1, v2)
-std::string mode = "double complex";
+std::string mode = "DC";
 int flopsPerCell = 8;
 
 #elif FR
@@ -26,7 +26,7 @@ typedef float dtype;
 dtype makeDtype(htype v) { return v; }
 #define RAND_HTYPE(gen) htype(gen)
 #define MAKE_DTYPE(v1, v2) float(v1)
-std::string mode = "float real";
+std::string mode = "FR";
 int flopsPerCell = 2;
 
 #elif DR
@@ -35,7 +35,7 @@ typedef double dtype;
 dtype makeDtype(htype v) { return v; }
 #define RAND_HTYPE(gen) htype(gen)
 #define MAKE_DTYPE(v1, v2) double(v1)
-std::string mode = "double real";
+std::string mode = "DR";
 int flopsPerCell = 2;
 
 #endif
