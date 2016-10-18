cce063206a163555bdd5ca0a94acdfd536a5b410
Dominik Ernst <dominik.ernst@fau.de>
2016-10-18 12:53:09 +0200

    benchdb: batch inserts, replace select/insert/update by insert or replace

--- benchdb.hpp
+++ benchdb.hpp
@@ -42,66 +42,30 @@ class BenchDB {
   void insert(int M, int N, std::string name, std::string mode, bool inplace,
               bool zerobeta, int K, double time, double flops, double bw) {
     _queue.push_back({M, N, name, mode, inplace, zerobeta, K, time, flops, bw});
-    if (_queue.size() > 50) {
-      int inserts = flush_entries();
-      std::cout << inserts << "*\n";
-    }
+    if (_queue.size() > 50) flush_entries();
   }
 
  private:
-  int flush_entries() {
+  void flush_entries() {
     sqlite3_exec(_dbCon, "BEGIN TRANSACTION", NULL, NULL, NULL);
-    int inserts = 0;
-    for (const auto& e : _queue) inserts += insert_entry(e);
-    std::cout << "Flush db: " << inserts << " new\n";
+    for (const auto& e : _queue) insert_entry(e);
     _queue.clear();
     sqlite3_exec(_dbCon, "COMMIT TRANSACTION", NULL, NULL, NULL);
-    return inserts;
   }
-  int insert_entry(const Entry& e) {
-    int inserts = 0;
-    if (_dbCon == NULL) return 0;
+  void insert_entry(const Entry& e) {
+    if (_dbCon == NULL) return;
     std::stringstream query;
 
-    query << "SELECT * FROM tsmm WHERE M=" << e.M << " AND N=" << e.N
-          << " AND name=\"" << e.name << "\" AND inplace=" << e.inplace
-          << " AND zerobeta=" << e.zerobeta;
+    query << "INSERT OR REPLACE INTO tsmm (M, N, name, inplace, zerobeta, K, "
+             "time, "
+             "flops, "
+             "bw) values("
+          << e.M << ", " << e.N << ", \"" << e.name << "\", " << e.inplace
+          << ", " << e.zerobeta << ", " << e.K << ", " << e.time << ", "
+          << e.flops << ", " << e.bw << ")";
 
     std::string queryStr = query.str();
-
-    int rowCount = 0;
-    sqlite3_error(sqlite3_exec(_dbCon, queryStr.c_str(),
-                               [](void* data, int rowCount, char**, char**) {
-                                 (*reinterpret_cast<int*>(data))++;
-                                 return 0;
-                               },
-                               reinterpret_cast<void*>(&rowCount), NULL));
-    // std::cout << queryStr << ": " << rowCount << " rows in Result\n";
-
-    if (rowCount > 0) {
-      query.str("");
-      query << "UPDATE tsmm set K=" << e.K << ", time=" << e.time
-            << ", flops=" << e.flops << ", bw=" << e.bw << " WHERE M=" << e.M
-            << " AND N=" << e.N << " AND name=\"" << e.name
-            << "\" AND inplace=" << e.inplace << " AND zerobeta=" << e.zerobeta;
-      queryStr = query.str();
-      // std::cout << queryStr << "\n";
-
-      sqlite3_error(sqlite3_exec(_dbCon, queryStr.c_str(), NULL, NULL, NULL));
-    } else {
-      query.str("");
-      query << "INSERT INTO tsmm (M, N, name, inplace, zerobeta, K, time, "
-               "flops, "
-               "bw) values("
-            << e.M << ", " << e.N << ", \"" << e.name << "\", " << e.inplace
-            << ", " << e.zerobeta << ", " << e.K << ", " << e.time << ", "
-            << e.flops << ", " << e.bw << ")";
-      queryStr = query.str();
-      // std::cout << queryStr << "\n";
-      inserts++;
-      sqlite3_error(sqlite3_exec(_dbCon, queryStr.c_str(), NULL, NULL, NULL));
-    }
-    return inserts;
+    sqlite3_error(sqlite3_exec(_dbCon, queryStr.c_str(), NULL, NULL, NULL));
   }
 
   void sqlite3_error(int result) {
