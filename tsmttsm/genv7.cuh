dc303abfbbabb44e7e9fac86a6463943915712c9
Dominik Ernst <dominik.ernst@fau.de>
2017-02-28 16:13:54 +0100

    genv7: 2x unrolling

--- tsmttsm/genv7.cuh
+++ tsmttsm/genv7.cuh
@@ -39,11 +39,13 @@ __global__ void blockProductKernel(const T *A, const T *B, iT *out, const int K,
   if (bOffset >= rowsPerBlock * ldb) bOffset = 0;
   if (aOffset >= rowsPerBlock * lda) aOffset = 0;
 
-  __shared__ iT
-      blockStorage[(rowsPerBlock + 1) * M * (sizeof(T) > sizeof(iT) ? 2 : 1)];
+  __shared__ iT blockStorage[2 * (rowsPerBlock + 1) * M *
+                             (sizeof(T) > sizeof(iT) ? 2 : 1)];
   T *rowCache = reinterpret_cast<T *>(blockStorage);
 
   zero(blockStorage[threadIdx.x]);
+  zero(blockStorage[threadIdx.x + BLOCKSIZE]);
+
   __syncthreads();
 
   iT threadSum[N];
@@ -52,30 +54,47 @@ __global__ void blockProductKernel(const T *A, const T *B, iT *out, const int K,
   }
 
   // Block synchronous loop
-  int idx = blockIdx.x * rowsPerBlock;
-  for (; idx < K - rowsPerBlock; idx += gridDim.x * rowsPerBlock) {
-    T av = A[idx * lda + aOffset];
-    T bv;
+  int idx = 2 * blockIdx.x * rowsPerBlock;
+  for (; idx < K - 2 * rowsPerBlock; idx += 2 * gridDim.x * rowsPerBlock) {
+    // if (threadIdx.x == 0)
+    //  printf("M  %d %d-%d\n", blockIdx.x, idx, idx + 2 * rowsPerBlock);
+    T av1 = A[idx * lda + aOffset];
+    T av2 = A[(idx + rowsPerBlock) * lda + aOffset];
+    T bv1 = av1;
+    T bv2 = av2;
 
     if (!SELF) {
-      bv = B[idx * ldb + bOffset];
-    } else {
-      bv = av;
+      bv1 = B[idx * ldb + bOffset];
+      bv2 = B[(idx + rowsPerBlock) * ldb + bOffset];
     }
 
     __syncthreads();
-    rowCache[threadIdx.x] = bv;
+    rowCache[threadIdx.x] = bv1;
+    rowCache[BLOCKSIZE + threadIdx.x] = bv2;
     __syncthreads();
 
     int localAddress = threadIdx.x - m;
     for (int n = 0; n < N; n++) {
-      threadSum[n] = axpy2(threadSum[n], av, rowCache[localAddress + n]);
+      threadSum[n] = axpy2(threadSum[n], av1, rowCache[localAddress + n]);
+      threadSum[n] =
+          axpy2(threadSum[n], av2, rowCache[BLOCKSIZE + localAddress + n]);
     }
   }
 
+  // if(threadIdx.x == 0) printf("   %d  @%d\n", blockIdx.x, idx);
   // Remainder loop
   for (idx = idx + localRow; idx < K; idx += gridDim.x * rowsPerBlock) {
+    // if (threadIdx.x == 0)
+    //  printf("R1 %d %d-%d\n", blockIdx.x, idx, idx + rowsPerBlock);
     T av = A[idx * lda + m];
+    for (int n = 0; n < N; n++) {
+      threadSum[n] = axpy2(threadSum[n], av, B[idx * ldb + n]);
+    }
+    idx += rowsPerBlock;
+    if (idx >= K) break;
+    // if (threadIdx.x == 0)
+    //  printf("R2 %d %d-%d\n", blockIdx.x, idx, idx + rowsPerBlock);
+    av = A[idx * lda + m];
     for (int n = 0; n < N; n++) {
       threadSum[n] = axpy2(threadSum[n], av, B[idx * ldb + n]);
     }
@@ -110,7 +129,7 @@ bool tsmttsm(int blockCount, const int varM, const int varN, const int K,
              const T *A, const int lda, const T alpha, const T *B,
              const int ldb, const T beta, T *C, const int ldc) {
   if (varM != M || varN != N) return false;
-  if (varM > 32 || varN > 32) return false;
+
   if (d_temp_storage == NULL)
     GPU_ERROR(cudaMalloc(&d_temp_storage, sizeof(iT) * 100 * 100 * 1000));
   if (blockCount * M * N > 100 * 100 * 1000) return false;
@@ -133,7 +152,7 @@ bool tsmttsm(int blockCount, const int varM, const int varN, const int K,
           A, B, (iT *)d_temp_storage, K, lda, ldb, ldc);
     }
   }
-  // GENV7::deviceReduce<T, iT, M, N><<<M * N / 256 + 1, 256>>>(
+  //GENV7::deviceReduce<T, iT, M, N><<<M * N / 256 + 1, 256>>>(
   //    (iT *)d_temp_storage, C, alpha, beta, blockCount, lda, ldb, ldc);
   return true;
 }
