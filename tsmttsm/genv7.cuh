503ddf0b33563ffcd3cd7988afab5db75a4cb69e
Dominik Ernst <dominik.ernst@fau.de>
2017-02-28 19:59:31 +0100

    genv7: per thread atomic reduction

--- tsmttsm/genv7.cuh
+++ tsmttsm/genv7.cuh
@@ -102,7 +102,7 @@ __global__ void blockProductKernel(const T *A, const T *B, iT *out, const int K,
   }
 
   // Calculate block results
-  for (int n = 0; n < N; n++) {
+  /*for (int n = 0; n < N; n++) {
     __syncthreads();
     blockStorage[threadIdx.x] = threadSum[n];
     __syncthreads();
@@ -118,6 +118,15 @@ __global__ void blockProductKernel(const T *A, const T *B, iT *out, const int K,
         multiArchAtomicAdd(out + m * ldc + n, NULL, blockSum);
       } else {
         multiArchAtomicAdd(out + n * ldc + m, NULL, blockSum);
+      }
+    }
+  }*/
+  if (localRow < rowsPerBlock) {
+    for (int n = 0; n < N; n++) {
+      if (TRANSPOSE) {
+        multiArchAtomicAdd(out + m * ldc + n, NULL, threadSum[n]);
+      } else {
+        multiArchAtomicAdd(out + n * ldc + m, NULL, threadSum[n]);
       }
     }
   }
