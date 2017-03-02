241d21abe5869ea5e7140289ac99bff76b45d821
Dominik Ernst <dominik.ernst@fau.de>
2017-03-02 17:08:26 +0100

    genv7: use correct dimension for block size calculation when transposing

--- tsmttsm/genv7.cuh
+++ tsmttsm/genv7.cuh
@@ -122,14 +122,14 @@ bool tsmttsm(int blockCount, const int varM, const int varN, const int K,
     GPU_ERROR(cudaMalloc(&d_temp_storage, sizeof(iT) * 100 * 100 * 1000));
   if (blockCount * M * N > 100 * 100 * 1000) return false;
 
-  int const blocksize = (256 / M) * M;
-
   if (N > M) {
+    int const blocksize = (256 / N) * N;
     GENV7::blockProductKernel<T, iT, N, M, blocksize, true,
                               false><<<blockCount, blocksize>>>(
         B, A, (iT *)d_temp_storage, K, ldb, lda, ldc);
 
   } else {
+    int const blocksize = (256 / M) * M;
     if (M == N && A == B) {
       GENV7::blockProductKernel<T, iT, M, N, blocksize, false,
                                 true><<<blockCount, blocksize>>>(
