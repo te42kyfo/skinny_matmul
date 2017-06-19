c975393816c99b1ae1ca48c70a311c5e90ccab02
Dominik Ernst <dominik.ernst@fau.de>
2017-06-19 20:33:13 +0200

    fixv3: interchange loops

--- tsmm/fix3.cuh
+++ tsmm/fix3.cuh
@@ -2,14 +2,13 @@
 #include "../eq.cuh"
 
 template <typename T, int M, int N, int BLOCKSIZE, bool BETAISZERO>
-static __global__ void tsmm_fix3_kernel(const T *__restrict__ A,
-                                        const T *__restrict__ B,
-                                        T *__restrict__ out, const int K,
-                                        const int lda, const int ldb,
-                                        const int ldc, T alpha, T beta) {
+static __global__ __launch_bounds__(BLOCKSIZE) void tsmm_fix3_kernel(
+    const T *__restrict__ A, const T *__restrict__ B, T *__restrict__ out,
+    const int K, const int lda, const int ldb, const int ldc, T alpha, T beta) {
   int tidx = blockIdx.x * BLOCKSIZE + threadIdx.x;
 
-  __shared__ T bCache[M][N];
+  __shared__ __volatile__ T bCache[M][N];
+#pragma unroll(1)
   for (int mn = threadIdx.x; mn < M * N; mn += BLOCKSIZE) {
     int tn = mn / M;
     int tm = mn % M;
@@ -19,21 +18,18 @@ static __global__ void tsmm_fix3_kernel(const T *__restrict__ A,
   __syncthreads();
 
   for (int row = tidx; row < K; row += gridDim.x * blockDim.x) {
-    T sums[N];
+
     for (int n = 0; n < N; n++) {
-      sums[n] = 0;
-    }
-    for (int m = 0; m < M; m++) {
-      for (int n = 0; n < N; n++) {
-        sums[n] = axpy(sums[n], A[row * lda + m], bCache[m][n]);
+      T sums;
+      for (int m = 0; m < M; m++) {
+        sums = axpy(sums, A[row * lda + m], bCache[m][n]);
       }
-    }
-    for (int n = 0; n < N; n++) {
+
       if (BETAISZERO) {
-        out[row * ldc + n] = scale(alpha, sums[n]);
+        out[row * ldc + n] = scale(alpha, sums);
       } else {
         out[row * ldc + n] =
-            axpby(sums[n], __ldg(out + row * ldc + n), alpha, beta);
+            axpby(sums, __ldg(out + row * ldc + n), alpha, beta);
       }
     }
   }
@@ -45,16 +41,22 @@ bool tsmm_fix3(const size_t blockCount, const int varM, const int varN,
                const T *B, const int ldb, const T beta, T *C, const int ldc) {
   if (varM != M || varN != N || A == C) return false;
 
-  const int BLOCKSIZE = 256;
+  const int BLOCKSIZE = 512;
+
+  struct cudaFuncAttributes funcAttrib;
+  cudaFuncGetAttributes(&funcAttrib,
+                        tsmm_fix3_kernel<T, M, N, BLOCKSIZE, false>);
+
+  //  std::cout << M << ": " << funcAttrib.numRegs << "\n";
 
   T Tzero;
   zero(Tzero);
   if (eq(beta, Tzero)) {
-    tsmm_fix3_kernel<T, M, N, BLOCKSIZE, true><<<blockCount, BLOCKSIZE>>>(
-        A, B, C, K, lda, ldb, ldc, alpha, beta);
+    tsmm_fix3_kernel<T, M, N, BLOCKSIZE, true>
+        <<<blockCount, BLOCKSIZE>>>(A, B, C, K, lda, ldb, ldc, alpha, beta);
   } else {
-    tsmm_fix3_kernel<T, M, N, BLOCKSIZE, false><<<blockCount, BLOCKSIZE>>>(
-        A, B, C, K, lda, ldb, ldc, alpha, beta);
+    tsmm_fix3_kernel<T, M, N, BLOCKSIZE, false>
+        <<<blockCount, BLOCKSIZE>>>(A, B, C, K, lda, ldb, ldc, alpha, beta);
   }
   return true;
 }
